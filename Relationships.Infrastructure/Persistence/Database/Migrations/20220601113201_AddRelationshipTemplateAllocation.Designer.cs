6a611659d02ca94b29b97e5f4cf60ad578219b18
Timo Notheisen <timo.notheisen@js-soft.com>
2022-06-01 14:32:05 +0200

    feat: add persistence configuration

--- Relationships.Infrastructure/Persistence/Database/Migrations/20220601113201_AddRelationshipTemplateAllocation.Designer.cs
+++ Relationships.Infrastructure/Persistence/Database/Migrations/20220601113201_AddRelationshipTemplateAllocation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Relationships.Infrastructure.Persistence.Database;
 
@@ -11,9 +12,10 @@ using Relationships.Infrastructure.Persistence.Database;
 namespace Relationships.Infrastructure.Persistence.Database.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601113201_AddRelationshipTemplateAllocation")]
+    partial class AddRelationshipTemplateAllocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
